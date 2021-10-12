3bb43077495f5ebbbe94b41a8daf629db700a328
JonathanLmao <Jonathan.woldu@elev.ga.lbs.se>
2021-10-12 12:00:58 +0200

    skapa egen scene
    vi borde alla skapa våra egna scener och sen merga de i slutet

--- Assets/BondeHit.cs
+++ Assets/BondeHit.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Bonde : MonoBehaviour
+public class BondeHit : MonoBehaviour
 {
+    public Text scoretext;
+    private void OnCollisionEnter(Collision collision)
+    {
+        scoretext.text = ("Score: " + 1);
+        new Vector3(0, -50, -20);
+    }
     // Start is called before the first frame update
     void Start()
     {
