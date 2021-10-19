7f4f75f1a4aa357d6188263383e99f2e987313b5
HackerMan761 <79506155+HackerMan761@users.noreply.github.com>
2021-10-19 10:57:13 +0200

    Movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,25 +4,31 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    //inte klar
-   [SerializeField]
-   int maxDistance;
-   private Rigidbody2D rbody2D;
-   void Start()
-   {
+    [SerializeField]
+    int speed;
+    [SerializeField]
+    int maxDistance;
+    private Rigidbody2D rbody2D;
+    void Start()
+    {
         rbody2D = GetComponent<Rigidbody2D>();
-   }
+    }
 
-   void Update()
-   {
-       if (Input.GetKey(KeyCode.D) && rbody2D.position.x <= maxDistance)
-       {
-            rbody2D.AddForce(new Vector2(50, 0));
-       }
-       if (Input.GetKey(KeyCode.A) && rbody2D.position.x >= -maxDistance)
-       {
-            rbody2D.AddForce(new Vector2(-50, 0));
-       }
-   }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) && rbody2D.position.x <= maxDistance)
+        {
+            rbody2D.velocity = (new Vector2(speed, 0));
+        }
+        else if (Input.GetKey(KeyCode.A) && rbody2D.position.x >= -maxDistance)
+        {
+            rbody2D.velocity = (new Vector2(-speed, 0));
+        }
+        else
+        {
+            rbody2D.velocity = Vector2.zero;
+        }
+        
+    }
 }
 
