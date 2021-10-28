26e76bdb0d2b0eabd40c260ec4cd990a3a7fc3e0
HackerMan761 <79506155+HackerMan761@users.noreply.github.com>
2021-10-28 20:11:31 +0200

    kanin gå animation

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public SpriteRenderer spriterenderer;
     [SerializeField]
     int speed;
     [SerializeField]
     int maxDistance;
     private Rigidbody2D rbody2D;
+    public Animator animator;
     void Start()
     {
         rbody2D = GetComponent<Rigidbody2D>();
@@ -19,16 +21,20 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.D) && rbody2D.position.x <= maxDistance)
         {
             rbody2D.velocity = (new Vector2(speed, 0));
+            animator.SetFloat("speed", 1);
+            spriterenderer.flipX = false;
         }
         else if (Input.GetKey(KeyCode.A) && rbody2D.position.x >= -maxDistance)
         {
             rbody2D.velocity = (new Vector2(-speed, 0));
+            animator.SetFloat("speed", 1);
+            spriterenderer.flipX = true;
         }
         else
         {
             rbody2D.velocity = Vector2.zero;
+            animator.SetFloat("speed", 0);
         }
-        
     }
 }
 
